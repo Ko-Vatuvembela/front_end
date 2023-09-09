03ab5c2a87493e07b6a6c16aedc28cdeca6e2ef0
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-09-09 13:16:12 -0300

    adjust the XL screen

--- src/app/components/shared/BackgroundImage.tsx
+++ src/app/components/shared/BackgroundImage.tsx
@@ -3,6 +3,12 @@ import { BackgroundImageType } from "../types";
 
 export const BackgroundImage = ({ source, width, height }: BackgroundImageType) => {
     return (
-        <Image alt="Background Image" src={"/images/" + source + ".svg"} width={width} height={height} className="opacity-30 right-[16.7%] fixed -mt-[10%] z-0" />
+        <Image
+            alt="Background Image"
+            src={"/images/" + source + ".svg"}
+            width={width}
+            height={height}
+            className="opacity-30 right-[16.7%] fixed -mt-[10%] z-0 xl:right-[5%] xl:-mt-[18%]"
+        />
     );
 };
