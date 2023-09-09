9f544ab5f91c2d24ab7b9a29e5d98a999816c73b
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-09-09 13:36:48 -0300

    adjust the XXL  screen

--- src/app/components/shared/BackgroundImage.tsx
+++ src/app/components/shared/BackgroundImage.tsx
@@ -3,6 +3,7 @@ import { BackgroundImageType } from "../types";
 
 export const BackgroundImage = ({ source, width, height }: BackgroundImageType) => {
     const baseStyle = 'opacity-30 right-[16.7%] fixed -mt-[10%] -z-10 right-[0%] ';
+    const xxl = '2xl:right-[16.7%] 2xl:-mt-[12.5%]';
     const xl = 'xl:-mt-[20%]';
     const lg = 'lg:-mt-[25%]';
 
@@ -12,7 +13,7 @@ export const BackgroundImage = ({ source, width, height }: BackgroundImageType) 
             src={"/images/" + source + ".svg"}
             width={width}
             height={height}
-            className={`${baseStyle} ${xl} ${lg}`}
+            className={`${baseStyle} ${xxl} ${xl} ${lg}`}
         />
     );
 };
