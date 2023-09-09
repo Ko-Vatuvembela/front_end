33eb429cb4bbc9f341cb954b6e2deb0c997ef72a
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-09-09 13:28:30 -0300

    adjust the LG  screen

--- src/app/components/shared/BackgroundImage.tsx
+++ src/app/components/shared/BackgroundImage.tsx
@@ -2,13 +2,17 @@ import Image from "next/image";
 import { BackgroundImageType } from "../types";
 
 export const BackgroundImage = ({ source, width, height }: BackgroundImageType) => {
+    const baseStyle = 'opacity-30 right-[16.7%] fixed -mt-[10%] -z-10 right-[0%] ';
+    const xl = 'xl:-mt-[20%]';
+    const lg = 'lg:-mt-[25%]';
+
     return (
         <Image
             alt="Background Image"
             src={"/images/" + source + ".svg"}
             width={width}
             height={height}
-            className="opacity-30 right-[16.7%] fixed -mt-[10%] z-0 xl:right-[5%] xl:-mt-[18%]"
+            className={`${baseStyle} ${xl} ${lg}`}
         />
     );
 };
