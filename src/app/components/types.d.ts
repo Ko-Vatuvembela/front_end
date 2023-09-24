f78aea9addaf673de7b5fe50d26b0fdbfae4c65b
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-09-24 17:18:40 -0300

    update gitignore

--- src/app/components/types.d.ts
+++ src/app/components/types.d.ts
@@ -1,37 +1,50 @@
+/* eslint-disable @typescript-eslint/member-delimiter-style */
 import { type ReactNode } from 'react';
 
 export interface InputType {
-    placeholder: string
-    name: string
-    label: string
-    isRequired: boolean
-    optinalStyle?: string
-    type: 'email' | 'password' | 'text'
+	placeholder: string;
+	name: string;
+	label: string;
+	isRequired: boolean;
+	optinalStyle?: string;
+	type: 'email' | 'password' | 'text';
 }
 
 export interface ButtonType {
-  text: string
-  style?: string
-  backgroundColor: string
-  textColor: string
-  image?: string
-  imageSide?: 'left' | 'right'
-  id?: string
-  isActive: boolean
-  hoverColor: string
+	text: string;
+	style?: string;
+	backgroundColor: string;
+	textColor: string;
+	image?: string;
+	imageSide?: 'left' | 'right';
+	id?: string;
+	isActive: boolean;
+	hoverColor: string;
 }
 export interface LinkType {
-  text: string
-  optinalStyle?: string
-  href: string
+	text: string;
+	optinalStyle?: string;
+	href: string;
 }
 export interface BackgroundImageType {
-  width: number
-  height: number
-  source: string
+	width: number;
+	height: number;
+	source: string;
 }
 export interface LayoutType {
-  backgroundImage: string
-  optionalStyle?: string
-  children: ReactNode
+	backgroundImage: string;
+	optionalStyle?: string;
+	children: ReactNode;
+}
+
+export interface UserType {
+	nome: string;
+	sobrenome: string;
+	email: string;
+	foto: string;
+	uid: number;
+}
+export interface SessionType {
+	token: string;
+	data: Date;
 }
