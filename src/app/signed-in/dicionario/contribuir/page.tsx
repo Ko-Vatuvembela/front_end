f67c523e065cfd9eda44d65f7c849e32f750973f
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-12-23 18:38:37 -0300

    chore: Run the linter

--- src/app/signed-in/dicionario/contribuir/page.tsx
+++ src/app/signed-in/dicionario/contribuir/page.tsx
@@ -1,13 +1,10 @@
 'use client';
 import { AuthProvider } from '@/app/context/AuthProvider';
-import { type LanguageType } from '@/app/components/types';
 import { LayoutPattern } from '@/app/public/LayoutPattern';
 import { useEffect, useState } from 'react';
-import FetchRequest from '@/app/provider/api';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { TextLink } from '@/app/components/shared/Link';
 
-export default function Contribuir() {
+export default function Contribuir () {
 	const router = useRouter();
 	const query = useSearchParams().get('languageID');
 	const [languageID, updateLanguage] = useState<number>();
