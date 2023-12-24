a9fd0d4d58f38d6f8dd24b83fad53d163ed257f1
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-12-24 19:30:41 -0300

    run the lint

--- src/app/signed-in/gramatica/page.tsx
+++ src/app/signed-in/gramatica/page.tsx
@@ -3,13 +3,13 @@ import { AuthProvider } from '@/app/context/AuthProvider';
 import { LayoutPattern } from '@/app/public/LayoutPattern';
 import { type LanguageType } from '@/app/components/types';
 import { useRouter } from 'next/navigation';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { categorias } from '@/app/components/shared/resources';
 import FetchRequest from '@/app/provider/api';
 
 const request = new FetchRequest();
 
-export default function NewComponent() {
+export default function NewComponent () {
 	const unselectedStyle =
 		'm-2 bg-slate-500 text-white p-3 rounded-lg border border-slate-500 hover:bg-white hover:cursor-pointer hover:text-slate-500';
 	const selectedStyle =
@@ -18,16 +18,19 @@ export default function NewComponent() {
 	const [langs, updateList] = useState<LanguageType[]>([]);
 
 	const [selectedLanguage, updateLanguage] = useState('Todas');
+	const selectedCategory = useRef('');
 
 	const selectLanguage = (index: number) => {
 		const linguasDiv = document.querySelector('.linguas');
 		if (linguasDiv) {
 			const selectedElement = linguasDiv.children[index];
-			selectedElement.className =
-				selectedElement.className === selectedStyle
-					? unselectedStyle
-					: selectedStyle;
-			updateLanguage(langs[index].lingua);
+			if (selectedElement.className === selectedStyle) {
+				selectedElement.className = unselectedStyle;
+				updateLanguage('Todas');
+			} else {
+				selectedElement.className = selectedStyle;
+				updateLanguage(langs[index].lingua);
+			}
 
 			for (let i = 0; i < linguasDiv.children.length; i++) {
 				const elem = linguasDiv.children[i];
@@ -72,14 +75,14 @@ export default function NewComponent() {
 								key={id}
 								onClick={() => {
 									selectLanguage(id);
-								}} // console.log(selectLanguage);
+								}}
 							>
 								{lingua}
 							</span>
 						))}
 					</div>
 					<p className="text-3xl font-light my-8 text-center">
-						Selecione o tópico
+						Clique sobre o tópico que deseja pesquisar
 					</p>
 					<div className="flex  flex-wrap justify-center">
 						{categorias.map(({ nome, descricao }, id) => {
@@ -87,6 +90,18 @@ export default function NewComponent() {
 								<div
 									className="max-w-sm rounded overflow-hidden hover:cursor-pointer hover:shadow-lg m-2"
 									key={id}
+									onClick={(e) => {
+										selectedCategory.current =
+											categorias[id].nome;
+										e.currentTarget.className =
+											'max-w-sm rounded overflow-hidden hover:cursor-pointer shadow-lg m-2';
+										setTimeout(() => {
+											console.log(
+												selectedLanguage,
+												selectedCategory.current
+											); // Redirecionar para a página
+										}, 1000);
+									}}
 								>
 									<div className="px-6 py-4">
 										<div className="font-bold text-xl mb-2">
