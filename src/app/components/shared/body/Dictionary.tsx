cecfaaa620998fdea78bf85031d8811a4f09811d
Rodolfo Cabral Neves <roddas360@gmail.com>
2024-01-16 01:28:24 -0300

    Add the dictionary, grammar and quotes initial components

---
+++ src/app/components/shared/body/Dictionary.tsx
@@ -0,0 +1,9 @@
+export const Dictionary = () => {
+	return (
+		<div className="w-full my-8">
+			<h1 className="text-3xl text-center text-black font-semibold my-8">
+				Dictionary
+			</h1>
+		</div>
+	);
+};
---
+++ src/app/components/shared/body/Grammar.tsx
@@ -0,0 +1,9 @@
+export const Grammar = () => {
+	return (
+		<div className="w-full my-8">
+			<h1 className="text-3xl text-center text-black font-semibold my-8">
+				Grammar
+			</h1>
+		</div>
+	);
+};
