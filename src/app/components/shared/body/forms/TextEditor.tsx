2c94a77cb8c1e1cd02b4d038814c674abf3e06ad
Rodolfo Cabral Neves <roddas360@gmail.com>
2024-01-16 03:46:29 -0300

    feat #12 : Add the grammar title

--- src/app/components/shared/body/forms/TextEditor.tsx
+++ src/app/components/shared/body/forms/TextEditor.tsx
@@ -1,11 +1,24 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { InputText } from './InputText';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import Editor from '../../../../../../ckeditor5/build/ckeditor';
 import parse from 'html-react-parser';
+import { h1 } from '../../resources';
 
 const TextEditor = () => {
-	const [text, updateText] = useState<string>();
+	const [text, setText] = useState<string>('');
+	const [title, setTitle] = useState<string>('');
+
+	useEffect(() => {
+		if (!text.length) {
+			setText('[Escreva alguma coisa]');
+		}
+		if (!title.length) {
+			setTitle('[Defina o título]');
+		}
+	}, [text, title]);
+
 	const styles = new Map<string, string>();
 
 	styles.set('h2', 'text-4xl font-bold');
@@ -17,14 +30,25 @@ const TextEditor = () => {
 
 	return (
 		<div className="App">
+			<InputText
+				isRequired={true}
+				label="Título"
+				name="title"
+				placeholder="Escreva o título da postagem"
+				onChange={(e) => {
+					setTitle(e);
+				}}
+				type="text"
+			/>
 			<CKEditor
 				editor={Editor}
 				data=""
 				onChange={(event, editor) => {
-					updateText(editor.data.get());
+					setText(editor.data.get());
 				}}
 			/>
 			<section className="my-10">
+				<h1 className={h1}>{title}</h1>
 				{text &&
 					parse(String(text), {
 						replace: (elem) => {
