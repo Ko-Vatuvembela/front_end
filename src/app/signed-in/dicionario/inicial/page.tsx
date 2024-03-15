1bb7fde95ef8f4bc51b69a7c78bb41376a7ec285
Rodolfo Cabral Neves <roddas360@gmail.com>
2024-03-15 02:31:40 -0300

    improvements

--- src/app/signed-in/dicionario/inicial/page.tsx
+++ src/app/signed-in/dicionario/inicial/page.tsx
@@ -14,6 +14,7 @@ import {
 	NOT_FOUND,
 	OK,
 	UNPROCESSABLE_ENTITY,
+	capitalize,
 } from '@/app/components/shared/resources';
 import { Back } from '@/app/components/shared/Back';
 import { Title } from '@/app/components/shared/Title';
@@ -80,7 +81,7 @@ export default function Inicial () {
 										<div key={id_palavra}>
 											<TextLink
 												href={`palavra?IDPalavra=${id_palavra}&IDLingua=${lingua_fk}`}
-												text={palavra}
+												text={capitalize(palavra)}
 											/>
 										</div>
 									)
